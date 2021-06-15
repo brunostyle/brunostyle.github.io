3bfd397110f917d57dfdae221024560c28d65f26
unknown <brunomstyle@gmail.com>
2021-06-15 19:41:22 -0300

    quinto commit

--- js/portafolio.js
+++ js/portafolio.js
@@ -7,25 +7,19 @@ const grid = new Muuri('.grid', {
 });
 
 window.addEventListener('load', () => {
-    grid.refreshItems().layout();
-    document.getElementById('grid').classList.add('imagenes-cargadas');
+  grid.refreshItems().layout();
+  document.getElementById('grid').classList.add('imagenes-cargadas');
 
-    // Agregamos los listener de los enlaces para filtrar por categoria.
+  // Agregamos los listener de los enlaces para filtrar por categoria.
 	const enlaces = document.querySelectorAll('#categorias a');
     enlaces.forEach((elemento) => {
 		  elemento.addEventListener('click', (evento) => {
-            evento.preventDefault();
-            enlaces.forEach((enlace) => enlace.classList.remove('activo'));
-            evento.target.classList.add('activo');
+        evento.preventDefault();
+        enlaces.forEach((enlace) => enlace.classList.remove('activo'));
+        evento.target.classList.add('activo');
             
-            const categoria = evento.target.innerHTML.toLowerCase();
-            categoria === 'todos' ? grid.filter('[data-categoria]') : grid.filter(`[data-categoria="${categoria}"]`);
+        const categoria = evento.target.innerHTML.toLowerCase();
+        categoria === 'todos' ? grid.filter('[data-categoria]') : grid.filter(`[data-categoria="${categoria}"]`);
 		  });
     });
-});
-
-const overlay = document.getElementById('overlay');
-document.querySelectorAll('.grid .item').forEach((elemento) => {
-  const descripcion = elemento.dataset.descripcion;
-  elemento.querySelector('#overlay .descripcion').innerHTML = descripcion;
 });
