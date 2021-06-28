b2dbf3e821d65b0a061a8238c5f05674959ab019
unknown <brunomstyle@gmail.com>
2021-06-27 23:42:02 -0300

    slide nuevo

--- js/portafolio.js
+++ js/portafolio.js
@@ -1,25 +1,13 @@
 /*----------------------------------------|PORTAFOLIO|----------------------------------------*/
 
-const grid = new Muuri('.grid', {
-    layout: {
-		rounding: false
-	}
-});
-
-window.addEventListener('load', () => {
-  grid.refreshItems().layout();
-  document.getElementById('grid').classList.add('imagenes-cargadas');
-
-  // Agregamos los listener de los enlaces para filtrar por categoria.
-	const enlaces = document.querySelectorAll('#categorias a');
-    enlaces.forEach((elemento) => {
-		  elemento.addEventListener('click', (evento) => {
-        evento.preventDefault();
-        enlaces.forEach((enlace) => enlace.classList.remove('activo'));
-        evento.target.classList.add('activo');
-            
-        const categoria = evento.target.innerHTML.toLowerCase();
-        categoria === 'todos' ? grid.filter('[data-categoria]') : grid.filter(`[data-categoria="${categoria}"]`);
-		  });
-    });
+var swiper = new Swiper(".portafolio__contenedor", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  spaceBetween: 42,
+  coverflowEffect: {
+    rotate: 0,
+  }
 });
