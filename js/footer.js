64582418cfea0b99b5151bf1061960f74a2d3ef0
unknown <brunomstyle@gmail.com>
2021-07-04 23:23:12 -0300

    4 de julio

---
+++ js/footer.js
@@ -0,0 +1,17 @@
+/*----------------------------------------|FOOTER|----------------------------------------*/
+
+var swiper = new Swiper(".footer__contenedor", {
+    slidesPerView: 1,
+    autoplay: true,
+    grabCursor: true,
+    loop: true,
+    breakpoints: {
+        510: {
+          slidesPerView: 2
+        },
+        800: {
+          slidesPerView: 3
+        }
+    }
+  });
+  
