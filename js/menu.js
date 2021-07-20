f1a0eafedc72bc6668867da5a4dda0b2892ff4db
unknown <brunomstyle@gmail.com>
2021-07-20 03:57:48 -0300

    portfolio with sass

--- js/menu.js
+++ js/menu.js
@@ -1,29 +1,24 @@
-/*----------------------------------------|MENU|----------------------------------------*/
 
-let menu = document.getElementById('menu');
-let menuLista = document.getElementById('menu-lista'),
-    menuBoton = document.getElementById('menu-boton');
+// Menu
+let headerMenu = document.getElementById('header-menu');
+let headerButton = document.getElementById('header-button');
+let headerLinks = document.querySelectorAll('.header__link');
 
-menuBoton.addEventListener('click', () => {
-    menu.classList.toggle('activo');
-    menuLista.classList.toggle('activo');
-    menuBoton.classList.toggle('menu-active');
-})
-
-let botones = document.querySelectorAll('.menu__lista__opcion a');
-
-botones.forEach((elemento) => {
-    elemento.addEventListener('click', () => {
-        botones.forEach((seleccionado) => {
-            seleccionado.classList.remove('activo');
-        });
-        elemento.classList.add('activo');
-        menuLista.classList.remove('activo');
-        menuBoton.classList.toggle('menu-active');
-    })
+headerButton.addEventListener('click', () => {
+    headerMenu.classList.toggle('active');
+    headerButton.classList.toggle('active');
 });
 
-/* ACTIVO EL LINK CUANDO EL SCROLL ESTE ENCIMA DE UNA SECCION */
+headerLinks.forEach(element => {
+    element.addEventListener('click', () => {
+        headerLinks.forEach(item => {
+            item.classList.remove('active');
+        });
+        element.classList.add('active');
+        headerMenu.classList.remove('active');
+        headerButton.classList.remove('active');
+    });
+});
 
 let sections = document.querySelectorAll('section[id]');
 
@@ -32,15 +27,33 @@ function scrollActive () {
 
     sections.forEach(current => {
         let sectionHeight = current.offsetHeight;
-        let sectionTop = current.offsetTop - 50;
+        let sectionTop = current.offsetTop - 60;
         sectionId = current.getAttribute('id');
         
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.menu__lista__opcion a[href*=' + sectionId + ']').classList.add('activo');
+            document.querySelector('.header__item a[href*=' + sectionId + ']').classList.add('active');
         } else {
-            document.querySelector('.menu__lista__opcion a[href*=' + sectionId + ']').classList.remove('activo');
+            document.querySelector('.header__item a[href*=' + sectionId + ']').classList.remove('active');
         }
     });
 }
 
 window.addEventListener('scroll', scrollActive);
+
+// Button Up
+let botonUp = document.getElementById('button-up');
+
+botonUp.addEventListener('click', () => {
+        window.scrollTo(0, 0);
+});
+
+window.onscroll = function () {
+    
+    let scroll = document.documentElement.scrollTop;
+
+    if(scroll > 300){
+        botonUp.classList.add('active');
+    } else if(scroll < 300){
+        botonUp.classList.remove('active');
+    }
+}
--- js/servicios.js
+++
@@ -1,9 +0,0 @@
-/*----------------------------------------|SERVICIOS|----------------------------------------*/
-
-let servicios = document.querySelectorAll('.servicios__contenedor a');
-
-servicios.forEach((elemento) => {
-    elemento.addEventListener('click', (e) => {
-        e.preventDefault();
-    })
-});
